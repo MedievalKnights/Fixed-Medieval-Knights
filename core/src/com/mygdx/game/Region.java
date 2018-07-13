08806f282814bfb4412c5b90e70eaf2442dd3b5d
apaara11 <league@WTS5.attlocal.net>
2018-07-12 18:23:02 -0700

    Finished Perlin

--- core/src/com/mygdx/game/Region.java
+++ core/src/com/mygdx/game/Region.java
@@ -14,10 +14,11 @@ public class Region {
 
 	Animation<TextureRegion> background = new Animation<TextureRegion>(1f, atlasBack.getRegions());
 	OpenSimplexNoise noise = new OpenSimplexNoise();
+	int seed = 14433;
 	int num = 0;
-	float scale = 5;
+	float scale = 20;
 	int[] keyFrameValues = { 1, 2, 0, 5, 4, 3 };
-	float[] threshholdValues = { 0.5f, 0.3f, 0.1f, 0,-0.2f};
+	float[] threshholdValues = { 0.45f, 0.3f, 0.1f, 0,-0.2f};
 	public static final int RESX = 16;
 	public static final int RESY = 16;
 	int regionX;
@@ -29,14 +30,14 @@ public class Region {
 		regionY = rY;
 		for (int i = 0; i < RESX; i++) {
 			for (int j = 0; j < RESY; j++) {
-				double value = noise.eval((i + (rX / 64)) / scale, (j + (rY / 64)) / scale, 0.0);
+				double value = noise.eval((i + (rX / 64)) / scale+seed, (j + (rY / 64)) / scale+seed, 0.0);
 				for (int k = 0; k < threshholdValues.length; k++) {
 					if (value > threshholdValues[k]) {
 						region[i][j] = new Tile(i * 64 + regionX, j * 64 + regionY, background.getKeyFrame(keyFrameValues[k]));
 						k = threshholdValues.length;
 					}
-					else if(k == threshholdValues.length-1) {
-						region[i][j] = new Tile(i * 64 + regionX, j * 64 + regionY, background.getKeyFrame(keyFrameValues[k]));
+					else if(k+1 == threshholdValues.length) {
+						region[i][j] = new Tile(i * 64 + regionX, j * 64 + regionY, background.getKeyFrame(keyFrameValues[k+1]));
 					}
 				}
 			}
