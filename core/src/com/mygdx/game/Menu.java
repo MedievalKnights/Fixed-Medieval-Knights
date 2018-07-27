8aec2f6bc592136d32416461dc9e21401576df6a
TheTrueTechie <shiva.malhotra.email@gmail.com>
2018-07-26 18:07:16 -0700

    Added Option and Exit Button
    Used place holder art for now. Buttons have no function at the moment.

--- core/src/com/mygdx/game/Menu.java
+++ core/src/com/mygdx/game/Menu.java
@@ -19,12 +19,16 @@ public class Menu {
 	TextButtonStyle buttonStyle;
 	BitmapFont font;
 	Skin skin;
+	Skin skin2;
+	Skin skin3;
 	TextureAtlas buttonAtlas;
 	SpriteBatch batch;
 	Texture background;
 	Pixmap pixmap;
 	Texture texture;
 	TextButton startButton;
+	TextButton optionButton; 
+	TextButton exitButton; 
 	
 	MedievalKnights game; 
 
@@ -34,35 +38,64 @@ public class Menu {
 		stage = new Stage();
 		font = new BitmapFont();
 		skin = new Skin();
+		skin2 = new Skin(); 
+		skin3 = new Skin(); 
 		
 		this.game = game; 
 		
 		Gdx.input.setInputProcessor(stage);
 
 		skin.add("default", font);
-		skin.add("startbutton", new Texture("buttons/startButton.jpg"));
+		skin2.add("default", font);
+		skin3.add("default", font);
+
+		
+		skin.add("startButton", new Texture("buttons/startButton.jpg"));
+		skin2.add("optionButton", new Texture("buttons/optionButton.jpg"));
+		skin3.add("exitButton", new Texture("buttons/exitButton.jpg"));
+		
 
 		TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-
-		textButtonStyle.up = skin.newDrawable("startbutton", Color.GRAY);
-		textButtonStyle.down = skin.newDrawable("startbutton", Color.DARK_GRAY);
-		textButtonStyle.over = skin.newDrawable("startbutton", Color.LIGHT_GRAY);
+		TextButton.TextButtonStyle textButtonStyle2 = new TextButton.TextButtonStyle();
+		TextButton.TextButtonStyle textButtonStyle3 = new TextButton.TextButtonStyle();
+		
+		textButtonStyle.up = skin.newDrawable("startButton", Color.GRAY);
+		textButtonStyle.down = skin.newDrawable("startButton", Color.DARK_GRAY);
+		textButtonStyle.over = skin.newDrawable("startButton", Color.LIGHT_GRAY);
 		textButtonStyle.font = skin.getFont("default");
+		
+		textButtonStyle2.up = skin2.newDrawable("optionButton", Color.GRAY);
+		textButtonStyle2.down = skin2.newDrawable("optionButton", Color.DARK_GRAY);
+		textButtonStyle2.over = skin2.newDrawable("optionButton", Color.LIGHT_GRAY);
+		textButtonStyle2.font = skin2.getFont("default");
+
+		textButtonStyle3.up = skin3.newDrawable("exitButton", Color.GRAY);
+		textButtonStyle3.down = skin3.newDrawable("exitButton", Color.DARK_GRAY);
+		textButtonStyle3.over = skin3.newDrawable("exitButton", Color.LIGHT_GRAY);
+		textButtonStyle3.font = skin3.getFont("default");
+
+		
 
 		skin.add("default", textButtonStyle);
+		skin2.add("default", textButtonStyle2);
+		skin3.add("default", textButtonStyle3);
 
-		pixmap = new Pixmap((int) Gdx.graphics.getWidth() / 4, (int) Gdx.graphics.getHeight() / 10,
-				Pixmap.Format.RGB888);
-		pixmap.setColor(Color.BLACK);
-		pixmap.fill();
 
-		//texture = new Texture(pixmap);
 		texture = new Texture("temp/background.jpg");
+		
 		startButton = new TextButton("", skin);
-		startButton.setBounds(Gdx.graphics.getWidth()/2-128, Gdx.graphics.getHeight()/2-64, 256, 128);
+		optionButton = new TextButton("", skin2);
+		exitButton = new TextButton("", skin3);
+		
+		startButton.setBounds(Gdx.graphics.getWidth()/2-128, Gdx.graphics.getHeight()*0.35f-64, 256, 128);
+		optionButton.setBounds(Gdx.graphics.getWidth()*0.25f-128, Gdx.graphics.getHeight()*0.15f-64, 256, 128);
+		exitButton.setBounds(Gdx.graphics.getWidth()*0.75f-128, Gdx.graphics.getHeight()*0.15f-64, 256, 128);
+
 		//startButton.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
 
 		stage.addActor(startButton);
+		stage.addActor(optionButton);
+		stage.addActor(exitButton);
 
 	}
 
