8eef2a387b1969e0680b7052268585ce009aefe7
Cole Rudnick <rudnickcole@gmail.com>
2018-07-26 17:59:09 -0700

    Random
    I randomized the seeds but its kinda broken

--- core/src/com/mygdx/game/Region.java
+++ core/src/com/mygdx/game/Region.java
@@ -14,30 +14,39 @@ public class Region {
 
 	Animation<TextureRegion> background = new Animation<TextureRegion>(1f, atlasBack.getRegions());
 	OpenSimplexNoise noise = new OpenSimplexNoise();
-	int seed = 1253;
+	int seed;
+	double rand;
 	int num = 0;
 	float scale = 20;
 	int[] keyFrameValues = { 1, 2, 0, 5, 4, 3 };
-	float[] threshholdValues = { 0.45f, 0.3f, 0.1f, 0,-0.2f};
+	float[] threshholdValues = { 0.45f, 0.3f, 0.1f, 0, -0.2f };
 	public static final int RESX = 16;
 	public static final int RESY = 16;
 	int regionX;
 	int regionY;
 	Tile[][] region = new Tile[RESX][RESY];
 
+	public void getRandom() {
+		rand = Math.random() * 10000;
+		seed = (int) rand;
+	}
+
 	public Region(int rX, int rY) {
+		getRandom();
+
 		regionX = rX;
 		regionY = rY;
 		for (int i = 0; i < RESX; i++) {
 			for (int j = 0; j < RESY; j++) {
-				double value = noise.eval((i + (rX / 64)) / scale+seed, (j + (rY / 64)) / scale+seed, 0.0);
+				double value = noise.eval((i + (rX / 64)) / scale + seed, (j + (rY / 64)) / scale + seed, 0.0);
 				for (int k = 0; k < threshholdValues.length; k++) {
 					if (value > threshholdValues[k]) {
-						region[i][j] = new Tile(i * 64 + regionX, j * 64 + regionY, background.getKeyFrame(keyFrameValues[k]),false);
+						region[i][j] = new Tile(i * 64 + regionX, j * 64 + regionY,
+								background.getKeyFrame(keyFrameValues[k]), false);
 						k = threshholdValues.length;
-					}
-					else if(k+1 == threshholdValues.length) {
-						region[i][j] = new Tile(i * 64 + regionX, j * 64 + regionY, background.getKeyFrame(keyFrameValues[k+1]),true);
+					} else if (k + 1 == threshholdValues.length) {
+						region[i][j] = new Tile(i * 64 + regionX, j * 64 + regionY,
+								background.getKeyFrame(keyFrameValues[k + 1]), true);
 					}
 				}
 			}
@@ -52,11 +61,11 @@ public class Region {
 			}
 		}
 	}
+
 	public boolean canBeWalkedOn(int x, int y) {
-		if(region[x][y].isObstacle) {
+		if (region[x][y].isObstacle) {
 			return false;
-		}
-		else {
+		} else {
 			return true;
 		}
 	}
